b688b33d1872285561d7beadedbec63552e1969d
Liam Rayback <liam@macbook.local>
2024-10-17 07:54:43 -0600

    added motorbike and some work on CLI and index

--- challenge-08/src/classes/Motorbike.ts
+++ challenge-08/src/classes/Motorbike.ts
@@ -1,12 +1,10 @@
 // Importing Vehicle and Wheel classes
+import { get } from 'http';
 import Vehicle from './Vehicle.js';
 import Wheel from './Wheel.js';
 
-// TODO: The Motorbike class should extend the Vehicle class
+
 class Motorbike extends Vehicle {
-  // TODO: Declare properties of the Motorbike class
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, and wheels
-  // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[])
   vin: string;
   color: string;
   make: string;
@@ -16,10 +14,6 @@ class Motorbike extends Vehicle {
   topSpeed: number;
   wheels: Wheel[];
 
-  // TODO: Create a constructor that accepts the properties of the Motorbike class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Motorbike class
-    // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
   constructor(
     vin: string,
     color: string,
@@ -47,11 +41,22 @@ class Motorbike extends Vehicle {
 
   // TODO: Implement the wheelie method
     // TODO: The method should log the message "Motorbike [make] [model] is doing a wheelie!"
+    wheelie(): void {
+     console.log(`Motorbike ${this.make} ${this.model} is doing a wheelie!`);
+    }
 
-  // TODO: Override the printDetails method from the Vehicle class
-  // TODO: The method should call the printDetails method of the parent class
-  // TODO: The method should log the details of the Motorbike
-  // TODO: The details should include the VIN, make, model, year, weight, top speed, color, and wheels
+    override printDetails(): void {
+      super.printDetails();
+      console.log(`VIN: ${this.vin}`);
+      console.log(`Make: ${this.make}`);
+      console.log(`Model: ${this.model}`);
+      console.log(`Year: ${this.year}`);
+      console.log(`weight: ${this.weight}`);
+      console.log(`topSpeed: ${this.topSpeed}`);
+      console.log(`color: ${this.color}`);
+      console.log(`wheel 1: ${this.wheels[0].getDiameter} inch tire with a ${this.wheels[0].getTireBrand} brand`);
+      console.log(`wheel 2: ${this.wheels[1].getDiameter} inch tire with a ${this.wheels[1].getTireBrand} brand`);
+    }
 }
 
 // Export the Motorbike class as the default export
